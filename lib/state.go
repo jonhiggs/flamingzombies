13b55320d54e3d7aa7b795e215e0c5b36766d0a3
Jon Higgs <jhiggs@eml.cc>
2024-04-11 08:50:18 +1000

    Hold more state in Task struct

--- lib/state.go
+++ lib/state.go
@@ -1,85 +1,25 @@
 package fz
 
-import "fmt"
-
-type StateRecord struct {
-	Hash   uint32
-	Status bool
-}
-
-type State struct {
-	Hash    uint32
-	Retries int
-	History uint32
-}
-
-var States []State
-var StateRecordCh = make(chan StateRecord, 100)
-
-func RecordStates() {
-	go func() {
-		for {
-			select {
-			case r := <-StateRecordCh:
-				st := FindState(r.Hash)
-				statusA := st.Status()
-				st.Append(r.Status)
-				st = FindState(r.Hash)
-				statusB := st.Status()
-
-				fmt.Printf("recording status of %d: %b: %b -> %b\n", st.Hash, st.History, statusA, statusB)
-
-				if statusA != statusB {
-					// TODO: signal the notification channel
-					fmt.Printf(
-						"status of %d has changed from %d to %d\n",
-						st.Hash,
-						statusA,
-						statusB,
-					)
-				}
-			}
-		}
-	}()
-}
-
-func FindState(hash uint32) *State {
-	for i, st := range States {
-		if st.Hash == hash {
-			return &States[i]
-		}
-	}
-
-	panic("state could not be found")
-}
-
-func (st *State) Append(b bool) {
-	st.History = st.History << 1
-	if b {
-		st.History += 1
-	}
-}
-
 // the returned values are:
 //
 //	-1: unknown
 //	 0: down
 //	 1: up
-func (st State) Status() int {
-	var mask uint32
-	for i := 0; i < st.Retries; i++ {
-		mask = mask << 1
-		mask += 1
-	}
-	v := st.History & mask
-
-	if v == 0 {
-		return 0
-	}
-
-	if v == mask {
-		return 1
-	}
-
-	return -1
-}
+//func (st State) Status() int {
+//	var mask uint32
+//	for i := 0; i < st.Retries; i++ {
+//		mask = mask << 1
+//		mask += 1
+//	}
+//	v := st.History & mask
+//
+//	if v == 0 {
+//		return 0
+//	}
+//
+//	if v == mask {
+//		return 1
+//	}
+//
+//	return -1
+//}
--- lib/task.go
+++ lib/task.go
@@ -15,15 +15,16 @@ var taskLocks []uint32
 var unlockLock sync.Mutex // ensure two unlocks don't run concurrently
 
 type Task struct {
-	Name      string
-	Command   string
-	Args      []string
-	Frequency int
-	Timeout   int
-	State     int
-	Notifiers []string
-	Retries   int
-	History   uint32 // represented in binary. sucessess are high
+	Name       string   // friendly name
+	Command    string   // command
+	Args       []string // command aruments
+	Frequency  int      // how often to run
+	Timeout    int      // how long an execution may run
+	LockTimout int      // how long to wait for a lock
+	Notifiers  []string
+	Retries    int
+	History    uint32     // represented in binary. sucessess are high
+	Mutex      sync.Mutex // lock to ensure one task runs at a time
 }
 
 func (t Task) Hash() uint32 {
@@ -42,14 +43,9 @@ func (t Task) Ready(ts time.Time) bool {
 }
 
 func (t *Task) Run() bool {
-	if t.isLocked() {
-		log.WithFields(log.Fields{
-			"file":      "lib/task.go",
-			"task_name": t.Name,
-			"task_hash": t.Hash(),
-		}).Info("aborting task because it is locked")
-		return true
-	}
+	// TODO: add a deadline
+	t.Mutex.Lock()
+	defer t.Mutex.Unlock()
 
 	log.WithFields(log.Fields{
 		"file":      "lib/task.go",
@@ -88,6 +84,11 @@ func (t *Task) RecordStatus(b bool) {
 		"task_name": t.Name,
 		"task_hash": t.Hash(),
 	}).Trace(fmt.Sprintf("history is %b", t.History))
+}
+
+// extract the current state from the History
+func (t *Task) State() int {
+	// TODO
 }
 
 func (t Task) isLocked() bool {
