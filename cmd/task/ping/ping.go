810253a870ad3cd58d0b02bfcd6266d9907cebb6
Jon Higgs <jhiggs@eml.cc>
2024-07-07 09:45:59 +1000

    Start building the pinger

--- cmd/task/ping/ping.go
+++ cmd/task/ping/ping.go
@@ -1,7 +1,90 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	probing "github.com/prometheus-community/pro-bing"
+	"nullprogram.com/x/optparse"
+)
+
+const (
+	DEFAULT_TIMEOUT_SECONDS = "5"
+	VERSION                 = "v0.0.0"
+)
+
+var (
+	count   int = 3
+	address string
+)
+
+func init() {
+	if os.Getenv("TIMEOUT") == "" {
+		os.Setenv("TIMEOUT", DEFAULT_TIMEOUT_SECONDS)
+	}
+
+	options := []optparse.Option{
+		{"count", 'c', optparse.KindRequired},
+		{"help", 'h', optparse.KindNone},
+		{"version", 'V', optparse.KindNone},
+	}
+
+	results, rest, err := optparse.Parse(options, os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, result := range results {
+		switch result.Long {
+		case "count":
+			var err error
+			count, err = strconv.Atoi(result.Optarg)
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+		case "help":
+			usage()
+			return
+		case "version":
+			fmt.Printf("fz %s\n", VERSION)
+			os.Exit(0)
+		}
+	}
+
+	if len(rest) != 1 {
+		fmt.Fprintln(os.Stderr, "Error: No hosts were provided")
+		os.Exit(1)
+	}
+
+	address = rest[0]
+}
 
 func main() {
-	fmt.Println("hello world")
+	pinger, err := probing.NewPinger(address)
+	if err != nil {
+		panic(err)
+	}
+	pinger.Count = count
+	err = pinger.Run() // Blocks until finished.
+	if err != nil {
+		panic(err)
+	}
+	stats := pinger.Statistics() // get send/receive/duplicate/rtt stats
+	fmt.Println(stats)
+}
+
+func usage() {
+	fmt.Println("Usage:")
+	fmt.Println("  ping [OPTIONS] HOST")
+	fmt.Println("")
+	fmt.Println("Options:")
+	fmt.Println("  -c, --count <number>           Number of ICMP packets to send")
+	fmt.Println("  -h, --help                     This help")
+	fmt.Println("  -V, --version                  Version")
+	os.Exit(0)
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,11 @@ require (
 	nullprogram.com/x/optparse v1.0.0
 )
 
-require github.com/cactus/go-statsd-client/v5 v5.1.0 // indirect
+require (
+	github.com/cactus/go-statsd-client/v5 v5.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/prometheus-community/pro-bing v0.4.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
