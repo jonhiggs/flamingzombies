7799fb12ac5537c53e38f7b47692589b513a6f7e
Jon Higgs <jhiggs@eml.cc>
2024-11-02 21:04:05 +1100

    Daemonise fz

--- cmd/fz/fz.go
+++ cmd/fz/fz.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/jonhiggs/flamingzombies/lib/daemon"
 	"github.com/jonhiggs/flamingzombies/lib/fz"
+	"github.com/sevlyar/go-daemon"
 	"nullprogram.com/x/optparse"
 )
 
@@ -15,6 +15,8 @@ const VERSION = "v0.0.21"
 
 var config fz.Config
 var configtestMode = false
+var foreground = false
+var pidfile string
 
 func init() {
 	if os.Getenv("FZ_CONFIG_FILE") == "" {
@@ -36,6 +38,7 @@ func init() {
 	options := []optparse.Option{
 		{"config", 'c', optparse.KindRequired},
 		{"configtest", 'n', optparse.KindNone},
+		{"foreground", 'F', optparse.KindNone},
 		{"directory", 'C', optparse.KindRequired},
 		{"help", 'h', optparse.KindNone},
 		{"loglevel", 'l', optparse.KindRequired},
@@ -60,12 +63,10 @@ func init() {
 			os.Setenv("FZ_LOG_LEVEL", result.Optarg)
 		case "directory":
 			os.Setenv("FZ_DIRECTORY", result.Optarg)
+		case "foreground":
+			foreground = true
 		case "pidfile":
-			err := os.WriteFile(result.Optarg, []byte(fmt.Sprintf("%d\n", os.Getpid())), 0644)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
+			pidfile = result.Optarg
 		case "statsd-host":
 			os.Setenv("FZ_STATSD_HOST", result.Optarg)
 		case "statsd-prefix":
@@ -94,12 +95,33 @@ func init() {
 
 	fz.ProcessNotifications()
 
-	if config.Listen() {
-		go daemon.Listen(&config)
-	}
+	//if config.Listen() {
+	//	go daemon.Listen(&config)
+	//}
 }
 
 func main() {
+	if !foreground {
+		cntxt := &daemon.Context{
+			PidFileName: pidfile,
+			PidFilePerm: 0644,
+			LogFileName: "sample.log", // TODO: get logfile
+			LogFilePerm: 0640,
+			WorkDir:     "./",
+			Umask:       027,
+			Args:        []string{"[fz]"},
+		}
+
+		d, err := cntxt.Reborn()
+		if err != nil {
+			log.Fatal("Unable to run: ", err)
+		}
+		if d != nil {
+			return
+		}
+		defer cntxt.Release()
+	}
+
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
@@ -123,6 +145,7 @@ func usage() {
 	fmt.Println("  -c, --config <file>            Configuration file")
 	fmt.Println("  -n, --configtest               Test validity of the configuration")
 	fmt.Println("  -C, --directory <path>         Change to directory")
+	fmt.Println("  -F, --foreground			      Run in foreground without daemonising")
 	fmt.Println("  -h, --help                     This help")
 	fmt.Println("  -l, --loglevel <level>         Override the log level")
 	fmt.Println("  -p, --pidfile                  The pidfile to write")
