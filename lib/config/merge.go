92fb09682dbccefadef45b04215e4fcc88f51300
Jon Higgs <jhiggs@eml.cc>
2025-01-07 07:49:49 +1100

    Move mergeEnvVars into merge.go

--- lib/config/merge.go
+++ lib/config/merge.go
@@ -1,3 +1,43 @@
 package config
 
+import "strings"
+
 // merge default configuration into a task, gate or notifier resource
+
+// Merge and deduplicate environment variables
+func mergeEnvVars(a, b []string) []string {
+	var res, keys []string
+	for _, s := range a {
+		found := false
+		k := strings.Split(s, "=")[0]
+
+		for _, ks := range keys {
+			if k == ks {
+				found = true
+			}
+		}
+
+		if !found {
+			keys = append(keys, k)
+			res = append(res, s)
+		}
+	}
+
+	for _, s := range b {
+		found := false
+		k := strings.Split(s, "=")[0]
+
+		for _, ks := range keys {
+			if k == ks {
+				found = true
+			}
+		}
+
+		if !found {
+			keys = append(keys, k)
+			res = append(res, s)
+		}
+	}
+
+	return res
+}
---
+++ lib/config/merge_test.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMergeEnvVars(t *testing.T) {
+	var tests = []struct {
+		a    []string
+		b    []string
+		want []string
+	}{
+		{ // 0
+			[]string{"A=1"},
+			[]string{"B=2"},
+			[]string{"A=1", "B=2"},
+		},
+		{ // 1
+			[]string{"A=1"},
+			[]string{"A=2"},
+			[]string{"A=1"},
+		},
+		{ // 1
+			[]string{"A=1"},
+			[]string{"B=2", "C=3"},
+			[]string{"A=1", "B=2", "C=3"},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			got := mergeEnvVars(tt.a, tt.b)
+			if fmt.Sprintf("%v", got) != fmt.Sprintf("%v", tt.want) {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
