7ee240d2a596c594db4108c14740dbe994f15510
Jon Higgs <jhiggs@eml.cc>
2025-01-11 07:42:59 +1100

    I don't think we need a special notification
    This can be done as part of the task execution

--- lib/fz/notification.go
+++ lib/fz/notification.go
@@ -1,119 +1,120 @@
 package fz
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/jonhiggs/flamingzombies/lib/config"
-)
-
-var NotifyCh = make(chan TaskNotification, 100)
-var ErrorNotifyCh = make(chan ErrorNotification, 100)
-
-func ProcessNotifications() {
-	go func() {
-		for {
-		C:
-			select {
-			case n := <-ErrorNotifyCh:
-				Logger.Info("sending error notification", "notifier", n.Notifier.Name)
-				n.Notifier.Execute(n.TraceID, n.Environment(), false)
-
-			case n := <-NotifyCh:
-				if !n.GateSetOpen() {
-					Logger.Debug("notification cancelled. all gatesets are closed.",
-						"notifier", n.Notifier.Name,
-						"task", n.Task.Name,
-						"trace_id", n.TraceID,
-					)
-					break C
-				}
-
-				n.Task.LastNotification = time.Now()
-				Logger.Info("sending notification",
-					"notifier", n.Notifier.Name,
-					"task", n.Task.Name,
-					"trace_id", n.TraceID,
-				)
-				err := n.Notifier.Execute(n.TraceID, n.Environment(&n.Task), true)
-				if err == nil {
-					n.UpdateLastNotification()
-				}
-			}
-		}
-	}()
-}
-
-// evaluate the state of the gatesets, and return true if any set is completely open.
-func (n TaskNotification) GateSetOpen() bool {
-	return gateSetOpen(&n.Task, n.Notifier.GateSets())
-}
-
-// evaluate the state of the gatesets, and return true if any set is completely open.
-func (n ErrorNotification) GateSetOpen() bool {
-	return gateSetOpen(&Task{TraceID: n.TraceID}, n.Notifier.GateSets())
-}
-
-// The environment variables provided to the notifiers
-func (n TaskNotification) Environment(tasks ...*config.Task) []string {
-	if len(n.Message) == 0 {
-		n.Message = "no message recieved"
-	}
-
-	v := []string{
-		fmt.Sprintf("MSG=%s", n.Message),
-		fmt.Sprintf("SUBJECT=%s: state is %s", n.Task.Name, n.Task.State()),
-		fmt.Sprintf("TASK_DESCRIPTION=%s", n.Task.description()),
-		fmt.Sprintf("TASK_DURATION_MS=%d", n.Duration.Milliseconds()),
-		fmt.Sprintf("TASK_EPOCH=%d", n.Timestamp.Unix()),
-		fmt.Sprintf("TASK_LAST_NOTIFICATION=%d", n.Task.LastNotification.Unix()),
-		fmt.Sprintf("TASK_LAST_STATE=%s", n.Task.LastState()),
-		fmt.Sprintf("TASK_NAME=%s", n.Task.Name),
-		fmt.Sprintf("TASK_PRIORITY=%d", n.Task.Priority),
-		fmt.Sprintf("TASK_STATE=%s", n.Task.State()),
-		fmt.Sprintf("TASK_TIMEOUT_MS=%d", n.Task.TimeoutSeconds*1000),
-		fmt.Sprintf("TASK_TRACE_ID=%s", n.Task.TraceID),
-	}
-
-	for _, t := range tasks {
-		v = MergeEnvVars(v, t.Envs)
-	}
-
-	v = MergeEnvVars(v, n.Notifier.Envs)
-	v = MergeEnvVars(v, cfg.Defaults.Envs)
-
-	return v
-}
-
-// Update the configuration's Task.
-// The task that's attached to the notification is only a copy.
-func (n TaskNotification) UpdateLastNotification() {
-	t := cfg.GetTaskByName(n.Task.Name)
-	t.LastNotification = time.Now()
-}
-
-// The environment variables provided to the error_notifiers
-func (n ErrorNotification) Environment() []string {
-	v := []string{
-		fmt.Sprintf("MSG=%s", n.Error),
-		fmt.Sprintf("SUBJECT=%s", "fz experienced a critical error"),
-		fmt.Sprintf("TASK_DESCRIPTION=%s", "An unexpected error occurred"),
-		fmt.Sprintf("TASK_TRACE_ID=%s", n.TraceID),
-	}
-
-	v = MergeEnvVars(v, n.Notifier.Envs)
-	v = MergeEnvVars(v, cfg.Defaults.Envs)
-
-	return v
-}
-
-func gateSetOpen(t *config.Task, gatesets [][]*config.Gate) bool {
-	for _, gs := range gatesets {
-		if GateSetOpen(t, gs...) {
-			return true
-		}
-	}
-
-	// no gatesets were open
-	return false
-}
+//
+//import (
+//	"fmt"
+//	"time"
+//
+//	"github.com/jonhiggs/flamingzombies/lib/config"
+//)
+//
+//var NotifyCh = make(chan TaskNotification, 100)
+//var ErrorNotifyCh = make(chan ErrorNotification, 100)
+//
+//func ProcessNotifications() {
+//	go func() {
+//		for {
+//		C:
+//			select {
+//			case n := <-ErrorNotifyCh:
+//				Logger.Info("sending error notification", "notifier", n.Notifier.Name)
+//				n.Notifier.Execute(n.TraceID, n.Environment(), false)
+//
+//			case n := <-NotifyCh:
+//				if !n.GateSetOpen() {
+//					Logger.Debug("notification cancelled. all gatesets are closed.",
+//						"notifier", n.Notifier.Name,
+//						"task", n.Task.Name,
+//						"trace_id", n.TraceID,
+//					)
+//					break C
+//				}
+//
+//				n.Task.LastNotification = time.Now()
+//				Logger.Info("sending notification",
+//					"notifier", n.Notifier.Name,
+//					"task", n.Task.Name,
+//					"trace_id", n.TraceID,
+//				)
+//				err := n.Notifier.Execute(n.TraceID, n.Environment(&n.Task), true)
+//				if err == nil {
+//					n.UpdateLastNotification()
+//				}
+//			}
+//		}
+//	}()
+//}
+//
+//// evaluate the state of the gatesets, and return true if any set is completely open.
+//func (n TaskNotification) GateSetOpen() bool {
+//	return gateSetOpen(&n.Task, n.Notifier.GateSets())
+//}
+//
+//// evaluate the state of the gatesets, and return true if any set is completely open.
+//func (n ErrorNotification) GateSetOpen() bool {
+//	return gateSetOpen(&Task{TraceID: n.TraceID}, n.Notifier.GateSets())
+//}
+//
+//// The environment variables provided to the notifiers
+//func (n TaskNotification) Environment(tasks ...*config.Task) []string {
+//	if len(n.Message) == 0 {
+//		n.Message = "no message recieved"
+//	}
+//
+//	v := []string{
+//		fmt.Sprintf("MSG=%s", n.Message),
+//		fmt.Sprintf("SUBJECT=%s: state is %s", n.Task.Name, n.Task.State()),
+//		fmt.Sprintf("TASK_DESCRIPTION=%s", n.Task.description()),
+//		fmt.Sprintf("TASK_DURATION_MS=%d", n.Duration.Milliseconds()),
+//		fmt.Sprintf("TASK_EPOCH=%d", n.Timestamp.Unix()),
+//		fmt.Sprintf("TASK_LAST_NOTIFICATION=%d", n.Task.LastNotification.Unix()),
+//		fmt.Sprintf("TASK_LAST_STATE=%s", n.Task.LastState()),
+//		fmt.Sprintf("TASK_NAME=%s", n.Task.Name),
+//		fmt.Sprintf("TASK_PRIORITY=%d", n.Task.Priority),
+//		fmt.Sprintf("TASK_STATE=%s", n.Task.State()),
+//		fmt.Sprintf("TASK_TIMEOUT_MS=%d", n.Task.TimeoutSeconds*1000),
+//		fmt.Sprintf("TASK_TRACE_ID=%s", n.Task.TraceID),
+//	}
+//
+//	for _, t := range tasks {
+//		v = MergeEnvVars(v, t.Envs)
+//	}
+//
+//	v = MergeEnvVars(v, n.Notifier.Envs)
+//	v = MergeEnvVars(v, cfg.Defaults.Envs)
+//
+//	return v
+//}
+//
+//// Update the configuration's Task.
+//// The task that's attached to the notification is only a copy.
+//func (n TaskNotification) UpdateLastNotification() {
+//	t := cfg.GetTaskByName(n.Task.Name)
+//	t.LastNotification = time.Now()
+//}
+//
+//// The environment variables provided to the error_notifiers
+//func (n ErrorNotification) Environment() []string {
+//	v := []string{
+//		fmt.Sprintf("MSG=%s", n.Error),
+//		fmt.Sprintf("SUBJECT=%s", "fz experienced a critical error"),
+//		fmt.Sprintf("TASK_DESCRIPTION=%s", "An unexpected error occurred"),
+//		fmt.Sprintf("TASK_TRACE_ID=%s", n.TraceID),
+//	}
+//
+//	v = MergeEnvVars(v, n.Notifier.Envs)
+//	v = MergeEnvVars(v, cfg.Defaults.Envs)
+//
+//	return v
+//}
+//
+//func gateSetOpen(t *config.Task, gatesets [][]*config.Gate) bool {
+//	for _, gs := range gatesets {
+//		if GateSetOpen(t, gs...) {
+//			return true
+//		}
+//	}
+//
+//	// no gatesets were open
+//	return false
+//}
--- lib/fz/notification_test.go
+++ lib/fz/notification_test.go
@@ -1,83 +1,83 @@
 package fz
 
-import (
-	"fmt"
-	"testing"
-	"time"
-
-	"github.com/jonhiggs/flamingzombies/lib/config"
-)
-
-var testTask = config.Task{
-	name:             "flappy",
-	description:      "a flappy task",
-	retries:          3,
-	lastNotification: time.Unix(0, 0),
-	traceID:          "123",
-}
-var testNotifier = Notifier{Name: "testing"}
-
-func TestNotificationEnvironment(t *testing.T) {
-	cfg.Defaults.Envs = []string{"MAIL_NAME=test@example"}
-	n := TaskNotification{
-		Notifier:  &testNotifier,
-		Task:      testTask,
-		Duration:  time.Second * 1,
-		Timestamp: time.Unix(1735517669, 0),
-	}
-
-	t.Run("env", func(t *testing.T) {
-
-		want := []string{
-			"MSG=no message recieved",
-			"SUBJECT=flappy: state is unknown",
-			"TASK_DESCRIPTION=a flappy task",
-			"TASK_DURATION_MS=1000",
-			"TASK_EPOCH=1735517669",
-			"TASK_LAST_NOTIFICATION=0",
-			"TASK_LAST_STATE=unknown",
-			"TASK_NAME=flappy",
-			"TASK_PRIORITY=0",
-			"TASK_STATE=unknown",
-			"TASK_TIMEOUT_MS=0",
-			"TASK_TRACE_ID=123",
-			"MAIL_NAME=test@example",
-		}
-		got := n.Environment(&n.Task)
-
-		if fmt.Sprintf("%v", got) != fmt.Sprintf("%v", want) {
-			t.Errorf("Expect '%v' but got '%v'", want, got)
-		}
-	})
-
-	// put the state back
-	cfg.Defaults.Envs = []string{}
-}
-
-func TestErrorNotificationEnvironment(t *testing.T) {
-	cfg.Defaults.Envs = []string{"MAIL_NAME=test@example"}
-	n := ErrorNotification{
-		Notifier: &testNotifier,
-		Error:    fmt.Errorf("this is an error"),
-		TraceID:  "ABC",
-	}
-
-	t.Run("env", func(t *testing.T) {
-
-		want := []string{
-			"MSG=this is an error",
-			"SUBJECT=fz experienced a critical error",
-			"TASK_DESCRIPTION=An unexpected error occurred",
-			"TASK_TRACE_ID=ABC",
-			"MAIL_NAME=test@example",
-		}
-		got := n.Environment()
-
-		if fmt.Sprintf("%v", got) != fmt.Sprintf("%v", want) {
-			t.Errorf("Expect '%v' but got '%v'", want, got)
-		}
-	})
-
-	// put the state back
-	cfg.Defaults.Envs = []string{}
-}
+//import (
+//	"fmt"
+//	"testing"
+//	"time"
+//
+//	"github.com/jonhiggs/flamingzombies/lib/config"
+//)
+//
+//var testTask = config.Task{
+//	name:             "flappy",
+//	description:      "a flappy task",
+//	retries:          3,
+//	lastNotification: time.Unix(0, 0),
+//	traceID:          "123",
+//}
+//var testNotifier = Notifier{Name: "testing"}
+//
+//func TestNotificationEnvironment(t *testing.T) {
+//	cfg.Defaults.Envs = []string{"MAIL_NAME=test@example"}
+//	n := TaskNotification{
+//		Notifier:  &testNotifier,
+//		Task:      testTask,
+//		Duration:  time.Second * 1,
+//		Timestamp: time.Unix(1735517669, 0),
+//	}
+//
+//	t.Run("env", func(t *testing.T) {
+//
+//		want := []string{
+//			"MSG=no message recieved",
+//			"SUBJECT=flappy: state is unknown",
+//			"TASK_DESCRIPTION=a flappy task",
+//			"TASK_DURATION_MS=1000",
+//			"TASK_EPOCH=1735517669",
+//			"TASK_LAST_NOTIFICATION=0",
+//			"TASK_LAST_STATE=unknown",
+//			"TASK_NAME=flappy",
+//			"TASK_PRIORITY=0",
+//			"TASK_STATE=unknown",
+//			"TASK_TIMEOUT_MS=0",
+//			"TASK_TRACE_ID=123",
+//			"MAIL_NAME=test@example",
+//		}
+//		got := n.Environment(&n.Task)
+//
+//		if fmt.Sprintf("%v", got) != fmt.Sprintf("%v", want) {
+//			t.Errorf("Expect '%v' but got '%v'", want, got)
+//		}
+//	})
+//
+//	// put the state back
+//	cfg.Defaults.Envs = []string{}
+//}
+//
+//func TestErrorNotificationEnvironment(t *testing.T) {
+//	cfg.Defaults.Envs = []string{"MAIL_NAME=test@example"}
+//	n := ErrorNotification{
+//		Notifier: &testNotifier,
+//		Error:    fmt.Errorf("this is an error"),
+//		TraceID:  "ABC",
+//	}
+//
+//	t.Run("env", func(t *testing.T) {
+//
+//		want := []string{
+//			"MSG=this is an error",
+//			"SUBJECT=fz experienced a critical error",
+//			"TASK_DESCRIPTION=An unexpected error occurred",
+//			"TASK_TRACE_ID=ABC",
+//			"MAIL_NAME=test@example",
+//		}
+//		got := n.Environment()
+//
+//		if fmt.Sprintf("%v", got) != fmt.Sprintf("%v", want) {
+//			t.Errorf("Expect '%v' but got '%v'", want, got)
+//		}
+//	})
+//
+//	// put the state back
+//	cfg.Defaults.Envs = []string{}
+//}
